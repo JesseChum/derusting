5a2a97fe8801c0f0258fb4f3f2cd647ef81079bc
JesseChum <jessechum@icloud.com>
2025-01-03 13:30:26 -0800

    Finished numberpractice and listpractice. commiting now as I forgot to do the same for my first file

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,28 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+  List<String> list = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+  list.add("Beans");
+  list.add("Pineapple");
+  list.add("onion");
     // Print the element at index 1
-
+  list.get(1);
+  System.out.println(list.get(1));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+  list.set(1, "Orange");
     // Insert a new element at index 0 (the length of the list will change)
-
+  list.add(0, "apple");
     // Check whether the list contains a certain string
-
+    if(list.contains("onion")) {
+      System.out.println("Found");
+    } else {
+      System.out.println("Not found");
+    }
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
+    for (int i = 0; i < list.size(); i++){
+      System.out.println("Index: " + i + ", Value: " + list.get(i));
+    }
 
     // Sort the list using the Collections library
-
+    Collections.sort(list);
     // Iterate over the list using a for-each loop
     // Print each value on a second line
-
+    for (String i : list){
+    System.out.println();
+    System.out.println(i);
+    }
     /*
      * Usage tip!
      * 
--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,18 +1,32 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float f = -1f;
+    System.out.println(f);
 
     // Create an int with a positive value and assign it to a variable
+    int num = 5;
+    System.out.println(num);
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    int remainder = num % 3;
+    System.out.println(remainder);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
-    // if the number is odd.
-
+    int number = 6;
+    if (number % 2 == 0) {
+      System.out.println(number + " is even");
+      // if the number is odd.
+    } else {
+      System.out.println(number + "is odd");
+    }
+    
     // Divide the number by another number using integer division
-
+    int x = 8;
+    int y = 4;
+    System.out.println(x/y);
     /*
      * Reminder!
      * 
