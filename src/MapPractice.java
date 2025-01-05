4c49d739f4acdad59c129b729376cf14daa09c81
JesseChum <jessechum@icloud.com>
2025-01-04 22:50:57 -0800

    Finished Set and Map practice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,29 +1,37 @@
-
+import java.util.HashMap;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
-
+  HashMap<String, Integer> people = new HashMap<String, Integer>();
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
-
+  people.put("John", 52);
+  people.put("Sarah", 43);
+  people.put("Charles", 23);
     // Get the value associated with a given key in the Map
-
+  people.get("Charles");
     // Find the size (number of key/value pairs) of the Map
-
+  people.size();
     // Replace the value associated with a given key (the size of the Map shoukld not change)
-
+  people.replace("john", 56);
     // Check whether the Map contains a given key
-
+  people.containsKey("sarah");
     // Check whether the Map contains a given value
-
+  people.containsValue("43");
     // Iterate over the keys of the Map, printing each key
-
+  for (String key : people.keySet()){
+    System.out.println(key);
+  }
     // Iterate over the values of the map, printing each value
-
+    for (Integer value: people.values()) {
+      System.out.println(value);
+    }
     // Iterate over the entries in the map, printing each key and value
-
+    for (String i : people.keySet()) {
+      System.out.println("key: " + i + " value: " + people.get(i));
+    }
     /*
      * Usage tip!
      * 
--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,18 +1,25 @@
+import java.util.HashSet;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
-
+    HashSet<String> helicopters = new HashSet<String>();
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
-
+    helicopters.add("Huey");
+    helicopters.add("Mi24");
+    helicopters.add("cobra");
     // Check whether the Set contains a given String
-
+    System.out.println("Does the set contain 'Mi24' ?" + helicopters.contains("Mi24"));
     // Remove an element from the Set
-
+    helicopters.remove("Huey");
     // Get the size of the Set
-
+    int size = helicopters.size();
+    System.out.println("The set size is: " + size);
     // Iterate over the elements of the Set, printing each one on a separate line
-
+    for (String helicopter : helicopters){
+      System.out.println(helicopter);
+    }
     /*
      * Warning!
      * 
